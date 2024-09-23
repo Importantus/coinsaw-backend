3fb644812de8d1cf4c7791545b13cbcf61102f1b
Importantus <importantus@mailbox.org>
2024-09-23 15:06:34 +0200

    🩹 Fix a bug where syncdate would be updated on already synced entries

--- src/models/data.ts
+++ src/models/data.ts
@@ -1,4 +1,4 @@
-import { Between } from "typeorm";
+import { Between, LessThanOrEqual, MoreThanOrEqual } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Entry } from "../entity/Entry";
 import { getDataQueryType } from "../validators/data";
@@ -14,7 +14,9 @@ export async function getDataFromGroup(groupId: string, query: getDataQueryType)
     const startDate = query.from ? new Date(parseInt(query.from)) : new Date(0);
     const endDate = query.to ? new Date(parseInt(query.to)) : new Date();
 
-    logger.debug(`Getting data from ${startDate} to ${endDate}`);
+    if (startDate && endDate && startDate > endDate) {
+        throw APIError.badRequest("From date is greater than to date");
+    }
 
     const data = await entryRepository.find({
         select: ["payload"],
@@ -35,13 +37,14 @@ export async function getDataFromGroup(groupId: string, query: getDataQueryType)
 
 }
 
-export async function createData(groupId: string, payload: string) {
+export async function createData(groupId: string, payload: any): Promise<string[]> {
     logger.debug(`Creating data for group ${groupId}`);
 
     const entryRepository = AppDataSource.getRepository(Entry);
 
     try {
         const entries = payload
+        const savedEntryIds = [];
 
         if (!Array.isArray(entries)) {
             throw Error();
@@ -52,13 +55,23 @@ export async function createData(groupId: string, payload: string) {
             if (!entry.id || !entry.timestamp || entry.groupId !== groupId) {
                 throw Error();
             } else {
-                logger.debug(new Date(entry.timestamp))
                 broadcast(groupId, JSON.stringify(entry));
                 const entryDB = await Entry.factory(entry.id, new Date(entry.timestamp), entry.groupId, JSON.stringify(entry));
-                await entryRepository.save(entryDB);
+                savedEntryIds.push(entry.id);
+                try {
+                    await entryRepository.insert(entryDB);
+                } catch (err) {
+                    if (err.code === '23505') {
+                        // Duplicate key error
+                        logger.info(`Entry with id ${entry.id} already exists in the database and is being skipped`);
+                    } else {
+                        throw err;
+                    }
+                }
             }
         }
 
+        return savedEntryIds;
     } catch (err) {
         logger.error(err);
         throw APIError.badRequest("Wrong payload format");
